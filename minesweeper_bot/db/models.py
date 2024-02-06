1e6917439b19eae0b7f5d54e07ef40db0e48b7ca
titovv <godlike200040@gmail.com>
2024-02-06 19:25:45 +0400

    Added db for minesweeper

---
+++ minesweeper_bot/db/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import BigInteger, Boolean, Column, DateTime, Integer
+from sqlalchemy.dialects.postgresql import UUID
+
+from db.base import Base
+
+
+class GameHistoryEntry(Base):
+    __tablename__ = "gamehistory"
+
+    game_id = Column(UUID, primary_key=True)
+    player_at = Column(DateTime, nullable=False)
+    telegram_id = Column(BigInteger, nullable=False, index=True)
+    field_size = Column(Integer, nullable=False)
+    victory = Column(Boolean, nullable=False)

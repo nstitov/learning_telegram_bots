f9623b8095a904bdd9a2cfc6acf323c01ffdf35b
Titov Nikita IDSR <godlike200040@gmail.com>
2024-02-09 17:40:53 +0400

    Added some tables to db.

--- finance_bot/database/db_init.py
+++ finance_bot/database/db_init.py
@@ -16,6 +16,17 @@ CREATE TABLE IF NOT EXISTS Categories (
 
 cursor.execute(
     """
+CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    telegram_id INTEGER NOT NULL,
+    user_name TEXT,
+    reg_date TEXT
+)
+"""
+)
+
+cursor.execute(
+    """
 CREATE TABLE IF NOT EXISTS Expenses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     expense_name TEXT,
@@ -29,15 +40,48 @@ cursor.execute(
     """
 CREATE TABLE IF NOT EXISTS Transactions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    cost REAL NOT NULL,
+    user_id INTEGER,
     expense_id INTEGER,
+    cost REAL NOT NULL,
     created_date TEXT,
     amount INTEGER DEFAULT 1,
     comment TEXT DEFAULT NULL,
-    FOREIGN KEY (expense_id) REFERENCES expences (id) ON DELETE CASCADE
+    FOREIGN KEY (expense_id) REFERENCES expences (id) ON DELETE RESTRICT,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 )
 """
 )
+
+cursor.execute(
+    """
+CREATE TABLE IF NOT EXSITS User_categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    category_id INTEGER,
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE RESTRICT,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+)
+"""
+)
+
+categories = [
+    "Продукты",
+    "Кафе",
+    "Рестораны",
+    "Жильё",
+    "Дорога",
+    "Машина",
+    "Счета",
+    "Образование",
+    "Здоровье",
+    "Игры",
+    "Подарки",
+    "Одежда",
+    "Досуг",
+    "Долг",
+    "Путешествия",
+    "Электроника",
+]
 
 connection.commit()
 connection.close()

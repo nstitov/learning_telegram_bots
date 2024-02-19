f73c7b83200d03de03c818ed925919cf8f354804
titovv <godlike200040@gmail.com>
2024-02-19 12:04:47 +0400

    Changed rep for finance telegram bot.

--- finance_bot/keyboards/cbdata.py
+++
@@ -1,5 +0,0 @@
-from aiogram.filters.callback_data import CallbackData
-
-
-class CategoriesCallbackFactory(CallbackData, prefix="add_category"):
-    category_name: str
--- finance_bot/lexicon/lexicon.py
+++
@@ -1,3 +0,0 @@
-from lexicon.lexicon_ru import LEXICON_RU
-
-translations = {"default": "ru", "ru": LEXICON_RU}
